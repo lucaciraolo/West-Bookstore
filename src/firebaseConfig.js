47a535fe4cc57c43f96272142f08b358c0662737
Luca Ciraolo <lucaciraolo@gmail.com>
2020-09-18 15:04:49 +0100

    added firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyD84urRVQAjRoQv23DHaQhTR4eQwklEN2Y",
+  authDomain: "west-bookstore.firebaseapp.com",
+  databaseURL: "https://west-bookstore.firebaseio.com",
+  projectId: "west-bookstore",
+  storageBucket: "west-bookstore.appspot.com",
+  messagingSenderId: "358345289936",
+  appId: "1:358345289936:web:b743c5fef93d5663692ed5",
+  measurementId: "G-Y5E1RKKVFF",
+};

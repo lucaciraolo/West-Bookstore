124627809d428fd6441fc44a98b53bb6b68da0a3
Ben Eastwood <ben.eastwood@bjss.com>
2020-09-18 16:33:06 +0100

    Added book names

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,15 +5,23 @@ import Title from "../components/Title";
 import BookCard from "../components/BookCard";
 
 const HomePage = () => {
-
   return (
       <Container breakpoint="widescreen">
         <Title title="Most popular books" />
         <Level>
-          <BookCard title={"Harry Potter"} rating={7} desc={"Harry potter is a good book"} />
-          <BookCard title={"Harry Potter"} rating={7} desc={"Harry potter is a good book"} />
-          <BookCard title={"Harry Potter"} rating={7} desc={"Harry potter is a good book"} />
-          <BookCard title={"Harry Potter"} rating={7} desc={"Harry potter is a good book"} />
+          {/* {books.map((book) => {
+            return (
+              <BookCard
+                title={book.title}
+                bookKey={book.bookId}
+                rating={book.rating}
+              />
+            );
+          })} */}
+          <BookCard title={"Harry Potter and the Philosopher's stone"} rating={7} desc={"Harry potter is a very good book"} />
+          <BookCard title={"Harry Potter and the Goblet of Fire"} rating={2} desc={"Harry potter is a medicore book"} />
+          <BookCard title={"Harry Potter and the Prisoner of Askaban"} rating={9} desc={"Harry potter is a excellent book"} />
+          <BookCard title={"Harry Potter and the Half Blood Prince"} rating={8} desc={"Harry potter is a good book"} />
         </Level>
       </Container>
   );

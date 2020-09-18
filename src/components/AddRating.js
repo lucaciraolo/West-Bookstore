b3c2179a25ddd549c5ac0de94a6cf6cfadc36818
Luca Ciraolo <lucaciraolo@gmail.com>
2020-09-18 16:20:53 +0100

    PUSHING

--- src/components/AddRating.js
+++ src/components/AddRating.js
@@ -7,41 +7,21 @@ import "firebase/firestore";
 import { Button } from "react-bulma-components";
 
 export default function AddRating({ bookKey }) {
-  const [rating] = useState([]);
-
-  const user = useContext(UserContext);
-
-  const setRating = (rating) => {
-    var db = firebaseProject.firestore();
-    db.collection("rating")
-      .add({
-        UserID: user.uid,
-        rating: rating,
-        BookID: bookKey,
-      })
-      .then(function (docRef) {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
-  };
-
   return (
     <div>
-      <Button className="btn danger" onClick={() => this.setRating(1)}>
+      <Button className="btn danger" onClick={() => setRating(1)}>
         1 Star
       </Button>
-      <Button className="btn danger" onClick={() => this.setRating(2)}>
+      <Button className="btn danger" onClick={() => setRating(2)}>
         2 Star
       </Button>
-      <Button className="btn danger" onClick={() => this.setRating(3)}>
+      <Button className="btn danger" onClick={() => setRating(3)}>
         3 Star
       </Button>
-      <Button className="btn danger" onClick={() => this.setRating(4)}>
+      <Button className="btn danger" onClick={() => setRating(4)}>
         4 Star
       </Button>
-      <Button className="btn danger" onClick={() => this.setRating(5)}>
+      <Button className="btn danger" onClick={() => setRating(5)}>
         5 Star
       </Button>
     </div>
--- src/components/rating/AddRating.js
+++ src/components/rating/AddRating.js
@@ -1,24 +1,45 @@
-import React, {useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../App";
+import { firebaseProject } from "../../firebase";
 
-import './AddRating.css'
+import "./AddRating.css";
 
-export default function AddRating(props) {
+export default function AddRating({ bookKey }) {
+  const user = useContext(UserContext);
 
-    const [rating] = useState([]);
+  const setRating = (rating) => {
+    var db = firebaseProject.firestore();
+    db.collection("rating")
+      .add({
+        UserID: user.uid,
+        rating: rating,
+        BookID: bookKey,
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  };
 
-    const setRating = (rating) => {
-        //getbookid from props passed in on declaration
-        console.log(props)
-        console.log(props.key);
-    }
-
-    return(
-        <div class="rating">
-            <button className="btn danger" onClick={() => setRating(1)}><span>☆</span></button>
-            <button className="btn danger" onClick={() => setRating(2)}><span>☆</span></button>
-            <button className="btn danger" onClick={() => setRating(3)}><span>☆</span></button>
-            <button className="btn danger" onClick={() => setRating(4)}><span>☆</span></button>
-            <button className="btn danger" onClick={() => setRating(5)}><span>☆</span></button>
-        </div>  
-    )
+  return (
+    <div class="rating">
+      <button className="btn danger" onClick={() => setRating(1)}>
+        <span>☆</span>
+      </button>
+      <button className="btn danger" onClick={() => setRating(2)}>
+        <span>☆</span>
+      </button>
+      <button className="btn danger" onClick={() => setRating(3)}>
+        <span>☆</span>
+      </button>
+      <button className="btn danger" onClick={() => setRating(4)}>
+        <span>☆</span>
+      </button>
+      <button className="btn danger" onClick={() => setRating(5)}>
+        <span>☆</span>
+      </button>
+    </div>
+  );
 }
